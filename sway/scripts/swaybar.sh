029e406170eb33e43c342836bc633c1d613a866f
XULIM <not3lbobillibo@gmail.com>
2024-06-03 17:22:49 -0400

    update regex for battery percentage in script

--- sway/scripts/swaybar.sh
+++ sway/scripts/swaybar.sh
@@ -1,5 +1,3 @@
-bt=$(acpi | grep -oe '[0-9]\{1,2\}%')
+bt=$(acpi | grep -oe '[0-9]\{1,3\}%')
 dt=$(date +'%Y-%m-%d %X')
-#lt=$(light)
-#sd=$(amixer sget Master | grep -oe '[0-9][0-9]%' -m 1)
 echo "${dt}   ${bt} "
